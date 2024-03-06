dcabef137988185df2aa40cb58d4faaf80b9880e
JongHo Park <jhpark3405@gmail.com>
2024-03-06 23:55:32 +0900

    ADD : data
    crate sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,3 @@
-spring.jpa.show-sql=true
-spring.jpa.properties.hibernate.format_sql=true
-spring.jpa.defer-datasource-initialization=true
-
 INSERT INTO member (id, name) VALUES (1, 'name1');
 INSERT INTO member (id, name) VALUES (2, 'name2');
 INSERT INTO member (id, name) VALUES (3, 'name3');
