17d2ec1bff7a61b11ee9b349e27fd56afb27b0a0
JongHo Park <jhpark3405@gmail.com>
2024-03-06 23:56:01 +0900

    ADD : DTO
    crate memberDTO

---
+++ src/main/java/helloworld/hellospring/DTO/MemberDTO.java
@@ -0,0 +1,30 @@
+package helloworld.hellospring.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDTO {
+
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
